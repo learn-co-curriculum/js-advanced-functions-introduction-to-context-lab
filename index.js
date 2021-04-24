8616b17a518ba036de9eefdb106863f693d58a5e
Hpollard003 <hpollard003@gmail.com>
2021-04-24 10:37:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,111 @@
 // Your code here
+//no i will not im going to put it \/HERE
+const createEmployeeRecord = (array) => {
+    let testEmployee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+}
+return testEmployee
+}
+
+function createEmployeeRecords(arrArr){
+    return arrArr.map(array => createEmployeeRecord(array))
+}
+
+function createTimeInEvent(){
+    let testEmployee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createTimeOutEvent(){}
+function hoursWorkedOnDate(){}
+function wagesEarnedOnDate(){}
+function allWagesFor(){}
+function calculatePayroll(){}
+function findEmployeeByFirstName(){}
+
+
+
+  // let employeeRecords = 
+    // {
+    //     'firstName': '',
+    //     'familyName': '',
+    //     'title': '',
+    //     'payPerHour': ${value}
+    // }
+    // const render = employeeRecords.map(record =>
+    //     if(record.firstName === '')
+    //     )
+
+    // let dataEmployees = [
+    //     {
+    //         'firstName': 'Thor',
+    //         'familyName': 'Odinsson',
+    //         'title': 'Electrical Engineer',
+    //         'payPerHour': 45     
+    //     },
+    //     {
+    //         'firstName': 'Loki',
+    //         'familyName': 'Laufrey-Odinson',
+    //         'title': 'HR Representative',
+    //         'payPerHour': 35     
+    //     },
+    //     {
+    //         'firstName': 'Natalia',
+    //         'familyName': 'Romanov',
+    //         'title': 'CEO',
+    //         'payPerHour': 150     
+    //     },
+    //     {
+    //         'firstName': 'Darcey',
+    //         'familyName': 'Lewis',
+    //         'title': 'Intern',
+    //         'payPerHour': 15     
+    //     },
+    //     {
+    //         'firstName': 'Jarvis',
+    //         'familyName': 'Stark',
+    //         'title': 'CIO',
+    //         'payPerHour': 125     
+    //     },
+    //     {
+    //         'firstName': 'Anthony',
+    //         'familyName': 'Stark',
+    //         'title': 'Angel Investor',
+    //         'payPerHour': 300     
+    //     },
+    //     {
+    //         'firstName': 'Byron',
+    //         'familyName': 'Poodle',
+    //         'title': 'Mascot',
+    //         'payPerHour': 3     
+    //     },
+    //     {
+    //         'firstName': 'Julius',
+    //         'familyName': 'Ceasar',
+    //         'title': 'General',
+    //         'payPerHour': 27     
+    //     }, 
+    //     {
+    //         'firstName': 'Rafiki',
+    //         'familyName': '',
+    //         'title': 'Aide',
+    //         'payPerHour': 10     
+    //     },
+    //     {
+    //         'firstName': 'Simba',
+    //         'familyName': '',
+    //         'title': 'King',
+    //         'payPerHour': 100     
+    //     }
+    //   ]
+    
