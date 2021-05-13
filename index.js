85b92026297689aaaaa84350e7c12ca76274f1d4
Charlene Johnson <charcodes24@gmail.com>
2021-05-12 17:52:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+function createEmployeeRecord(array) {
+    let employeeRecord = {
+        firstName: array[0], 
+        familyName: array[1], 
+        title: array[2], 
+        payPerHour: array[3], 
+        timeInEvents: [], 
+        timeOutEvents: []
+    };
+    return employeeRecord;
+}   
+
+function createEmployeeRecords(arrayOfArrays) {
+    let arrayOfEmployees = []
+    for (let array of arrayOfArrays) {
+        arrayOfEmployees.push(createEmployeeRecord(array));
+    }
+    return arrayOfEmployees;
+}
+
+function createTimeInEvent(employeeRecord, dateTime) {
+    let dateTimeArray = dateTime.split(' ');
+    let date = dateTimeArray[0];
+    let time = parseInt(dateTimeArray[1])
+    employeeRecord.timeInEvents.push({
+        'type': 'TimeIn',
+        'date': date, 
+        'hour': time
+    })
+    return employeeRecord;
+}
+
+function createTimeOutEvent(employeeRecord, dateTime) {
+    let dateTimeArray = dateTime.split(' ');
+    let date = dateTimeArray[0];
+    let time = parseInt(dateTimeArray[1])
+    employeeRecord.timeOutEvents.push({
+        'type': 'TimeOut',
+        'date': date, 
+        'hour': time
+    })
+    return employeeRecord;
+}
+
+function hoursWorkedOnDate(employeeRecord) {
+    if (employeeRecord.timeOutEvents['date'] === employeeRecord.timeInEvents['date']) {
+        return employeeRecord.timeOutEvents['time'] - employeeRecord.timeInEvents['time']
+    }
+}
